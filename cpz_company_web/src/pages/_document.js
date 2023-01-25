979bec060bfdefc5f8c5b35b285fa32a3a9ab2d7
SkyX [ID FR] <89273191+SkyX-ID-FR@users.noreply.github.com>
2023-01-25 14:57:51 +0100

    An project was born ! ✨

---
+++ cpz_company_web/src/pages/_document.js
@@ -0,0 +1,23 @@
+/* 
+  _______ _______ _______     _______  ___  ___ __      __       __ 
+ |   _   |   _   |   _   |   |   _   .'  _.'  _|__.----|__.-----|  |
+ |.  1___|.  1   |___|   |   |.  |   |   _|   _|  |  __|  |  -__|  |
+ |.  |___|.  ____|/  ___/    |.  |   |__| |__| |__|____|__|_____|__|
+ |:  1   |:  |   |:  1  \    |:  1   | Version 1.0                       
+ |::.. . |::.|   |::.. . |   |::.. . | By XlaTor & SkyX-ID-FR                               
+ `-------`---'   `-------'   `-------'   
+*/
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head />
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
+}
